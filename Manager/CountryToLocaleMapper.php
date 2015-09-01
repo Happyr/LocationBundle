1e2209aa950f9f94c1ad3c0771d015e35538a607
Tobias Nyholm <tobias.nyholm@gmail.com>
2015-09-01 19:30:30 +0200

    Added country to locale mapper

---
+++ Manager/CountryToLocaleMapper.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Happyr\LocationBundle\Manager;
+
+/**
+ * @author Tobias Nyholm
+ */
+class CountryToLocaleMapper
+{
+    /**
+     * Get the most likely locale from a ISO2 country code
+     *
+     * @param string $countryCode
+     *
+     * @return string
+     */
+    static function getLocale($countryCode)
+    {
+        switch ($countryCode) {
+            case 'SE':
+                return 'sv';
+            case 'RU':
+                return 'ru';
+            case 'DA':
+                return 'da';
+            default:
+                return 'en';
+        }
+    }
+}
